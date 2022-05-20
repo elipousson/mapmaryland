917a4d88f558c3516945d92bc0ef9c9e1e4c34c5
Eli Pousson <eli.pousson@gmail.com>
2022-05-20 13:27:02 -0400

    refactor: replace get_md_boundary w/ get_md_tigris
    - refactor: replace get_md_boundary w/ get_md_tigris
    - feat: add support to more data sources including sources downloaded w/ the tigris package
    - feat: add CRS support to get_md_tigris and erase_water option
    - feat: add format_md_sf function

---
+++ R/format_md_data.R
@@ -0,0 +1,43 @@
+#' Format Maryland simple feature data
+#'
+#' Convert the coordinate reference system, convert invalid geometry to valid
+#' geometry with [sf::st_make_valid], and, optionally, erase water, and clean
+#' names with [janitor::clean_names].
+#'
+#' @param data A `sf` object.
+#' @param crs Coordinate reference system passed to [sf::st_transform], Default: 3857
+#' @param erase_water If `TRUE`, use [overedge::st_erase] to erase any geometry intersecting with [md_water]; Default: `FALSE`
+#' @param clean_names If `TRUE`, use [janitor::clean_names]; Default: `TRUE`
+#' @inheritParams overedge::format_data
+#' @return A modified version of the input simple feature data.
+#' @rdname format_md_sf
+#' @seealso [overedge::format_data]
+#' @export
+#' @importFrom overedge is_sf is_same_crs st_erase rename_sf_col
+#' @importFrom sf st_transform st_is_valid st_make_valid
+format_md_sf <- function(data, crs = 3857, erase_water = FALSE, clean_names = TRUE, sf_col = "geometry") {
+
+  stopifnot(
+    overedge::is_sf(data)
+  )
+
+  if (clean_names) {
+    data <- janitor::clean_names(data)
+  }
+
+  if (!overedge::is_same_crs(data, crs)) {
+    data <- sf::st_transform(data, crs)
+  }
+
+  if (erase_water) {
+    data <- overedge::st_erase(data, mapmaryland::md_water)
+  }
+
+  if (!sf::st_is_valid(data)) {
+    data <- sf::st_make_valid(data)
+  }
+
+  data <- overedge::rename_sf_col(data, sf_col = "geometry")
+
+  return(data)
+}
---
+++ R/get_md_tigris.R
@@ -0,0 +1,69 @@
+#' Use package data or the {tigris} package to get Maryland data from the U.S. Census Bureau
+#'
+#' Get package data with Maryland counties, U.S. Congressional District, or Census designated
+#' places by name or GeoID or use the tigris package to download state data.
+#'
+#' @details Use {mapmaryland} packaged data or use tigris U.S. Census Bureau API:
+#'
+#' Supported options that access data included with the {mapmaryland} package
+#' include "counties", "census places", "congressional districts", "legislative
+#' districts", and "water".
+#'
+#' Options that access data by calling one of several tigris functions for
+#' downloading data from the U.S. Census Bureau API include "senate district",
+#' "tracts", "block groups", "blocks", "pumas", "voting districts". tigris
+#' functions that do not use a "state" parameter (e.g. [tigris::zctas])  are not
+#' supported by this function.
+#'
+#' @param name Name matching a value in the namelsad, namelsad, or geoid columns
+#'   of [md_counties], [md_census_places], [md_congressional_districts],
+#'   [md_legislative_districts], or [md_senate_districts],
+#'   Default: NULL
+#' @param type Type of data to return, Default: "counties"; See details for supported options.
+#' @param ... Additional parameters passed on to {tigris} functions.
+#' @return A simple feature object matching the type provided.
+#' @rdname get_md_tigris
+#' @export
+#' @importFrom tigris state_legislative_districts county_subdivisions block_groups blocks pumas voting_districts
+#' @importFrom janitor clean_names
+#' @importFrom overedge is_same_crs st_erase
+#' @importFrom sf st_transform
+get_md_tigris <- function(name = NULL, type = "counties", crs = 3857, erase_water = FALSE, ...) {
+
+  pkg_data <-  c("counties", "census places", "congressional districts", "legislative districts", "water")
+  api_data <- c("senate district", "tracts", "block groups", "blocks", "pumas", "voting districts")
+
+  type <-
+    match.arg(
+      tolower(type),
+      c("county", pkg_data, api_data)
+    )
+
+  state <- "MD"
+
+  data <-
+    switch(type,
+           "county" = mapmaryland::md_counties,
+           "counties" = mapmaryland::md_counties,
+           "census places" = mapmaryland::md_census_places,
+           "congressional districts" = mapmaryland::md_congressional_districts,
+           "legislative districts" = mapmaryland::md_legislative_districts,
+           "water" = mapmaryland::md_water,
+           "senate districts" = tigris::state_legislative_districts(state = state, house = "upper", ...),
+           "county subdivisions" = tigris::county_subdivisions(state = state, ...),
+           "block groups" = tigris::block_groups(state = state, ...),
+           "blocks" = tigris::blocks(state = state, ...),
+           "pumas" = tigris::pumas(state = state, ...),
+           "voting districts" = tigris::voting_districts(state = state, ...)
+    )
+
+  if (type %in% api_data) {
+    data <- janitor::clean_names(data)
+  }
+
+  data <- data[(data[["name"]] %in% name) | (data[["namelsad"]] %in% name) | (data[["geoid"]] %in% name), ]
+
+  data <- format_md_sf(data, crs = crs, erase_water = erase_water, clean_names = FALSE)
+
+  return(data)
+}
