5a189b1939685486627f453af19818212900855e
Eli Pousson <eli.pousson@gmail.com>
2022-06-01 18:51:44 -0400

    fix: update test for get_md_tigris

--- tests/testthat/test-get_md_boundary.R
+++
@@ -1,9 +0,0 @@
-test_that("get_md_boundary works", {
-  expect_s3_class(get_md_boundary("Baltimore city"), "sf")
-
-  expect_s3_class(get_md_boundary("Baltimore county"), "sf")
-
-  expect_s3_class(get_md_boundary("Hagerstown", type = "census place"), "sf")
-
-  expect_s3_class(get_md_boundary("1st District", type = "congressional district"), "sf")
-})
---
+++ tests/testthat/test-get_md_tigris.R
@@ -0,0 +1,9 @@
+test_that("get_md_tigris works", {
+  expect_s3_class(get_md_tigris("Baltimore city"), "sf")
+
+  expect_s3_class(get_md_tigris("Baltimore county"), "sf")
+
+  expect_s3_class(get_md_tigris("Hagerstown", type = "census places"), "sf")
+
+  expect_s3_class(get_md_tigris("1st District", type = "congressional districts"), "sf")
+})
