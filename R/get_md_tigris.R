fdc008844db1ff1558f91fa9dd4e61764076f95a
Eli Pousson <eli.pousson@gmail.com>
2022-06-13 23:10:46 -0400

    feat: expand get_md_tigris support for more data types

--- R/get_md_tigris.R
+++ R/get_md_tigris.R
@@ -1,24 +1,29 @@
 #' Use package data or the {tigris} package to get Maryland data from the U.S. Census Bureau
 #'
-#' Get package data with Maryland counties, U.S. Congressional District, or Census designated
-#' places by name or GeoID or use the tigris package to download state data.
+#' Get package data with Maryland counties, U.S. Congressional District, or
+#' Census designated places by name or GeoID or use the tigris package to
+#' download state data.
 #'
-#' @details Use {mapmaryland} packaged data or use tigris U.S. Census Bureau API:
+#' @details Use {mapmaryland} packaged data or use tigris U.S. Census Bureau
+#'   API:
 #'
-#' Supported options that access data included with the {mapmaryland} package
-#' include "counties", "census places", "congressional districts", "legislative
-#' districts", and "water".
+#'   Supported options that access data included with the {mapmaryland} package
+#'   include "counties", "census places", "congressional districts",
+#'   "legislative districts", and "water".
 #'
-#' Options that access data by calling one of several tigris functions for
-#' downloading data from the U.S. Census Bureau API include "senate district",
-#' "tracts", "block groups", "blocks", "pumas", "voting districts". tigris
-#' functions that do not use a "state" parameter (e.g. [tigris::zctas])  are not
-#' supported by this function.
+#'   Options that access data by calling one of several tigris functions for
+#'   downloading data from the U.S. Census Bureau API include "senate district",
+#'   "tracts", "block groups", "blocks", "pumas", "voting districts", "zctas",
+#'   "roads", "primary secondary roads", "area water",  "linear water", and
+#'   "landmarks".
+#'
+#'   tigris functions that do not use a "state" parameter (e.g.
+#'   [tigris::coastline] or [tigris::rails])  are not supported by this
+#'   function.
 #'
 #' @param name Name matching a value in the namelsad, namelsad, or geoid columns
-#'   of [md_counties], [md_census_places], [md_congressional_districts],
-#'   [md_legislative_districts], or one of the data sets available for download
-#'   with {tigris}; Default: `NULL`
+#'   of [md_counties], [md_census_places], [md_congressional_districts], or one
+#'   of the data sets available for download with {tigris}; Default: `NULL`
 #' @param type Type of data to return, Default: "counties"; See details for
 #'   supported options.
 #' @inheritParams format_md_sf
@@ -26,13 +31,18 @@
 #' @return A simple feature object matching the type provided.
 #' @rdname get_md_tigris
 #' @export
-#' @importFrom tigris state_legislative_districts county_subdivisions block_groups blocks pumas voting_districts
+#' @importFrom tigris state_legislative_districts county_subdivisions block_groups blocks pumas voting_districts roads primary_secondary_roads area_water linear_water landmarks zctas
 #' @importFrom janitor clean_names
-#' @importFrom overedge is_same_crs st_erase
-#' @importFrom sf st_transform
 get_md_tigris <- function(name = NULL, type = "counties", crs = getOption("mapmaryland.crs", default = 3857), erase_water = FALSE, ...) {
-  pkg_data <- c("counties", "census places", "congressional districts", "legislative districts", "water")
-  api_data <- c("senate district", "tracts", "block groups", "blocks", "pumas", "voting districts")
+  pkg_data <-
+    c("counties", "census places", "congressional districts", "water")
+
+  api_data <-
+    c(
+      "legislative districts", "senate district", "voting districts",
+      "tracts", "block groups", "blocks", "pumas", "zctas",
+      "roads", "primary secondary roads", "area water", "linear water", "landmarks"
+    )
 
   type <-
     match.arg(
@@ -44,25 +54,33 @@ get_md_tigris <- function(name = NULL, type = "counties", crs = getOption("mapma
 
   data <-
     switch(type,
-      "county" = mapmaryland::md_counties,
-      "counties" = mapmaryland::md_counties,
-      "census places" = mapmaryland::md_census_places,
-      "congressional districts" = mapmaryland::md_congressional_districts,
-      "legislative districts" = mapmaryland::md_legislative_districts,
-      "water" = mapmaryland::md_water,
+      "county" = md_counties,
+      "counties" = md_counties,
+      "census places" = md_census_places,
+      "congressional districts" = md_congressional_districts,
+      "water" = md_water,
+      "legislative districts" = tigris::state_legislative_districts(state = state, house = "lower", ...),
       "senate districts" = tigris::state_legislative_districts(state = state, house = "upper", ...),
       "county subdivisions" = tigris::county_subdivisions(state = state, ...),
       "block groups" = tigris::block_groups(state = state, ...),
       "blocks" = tigris::blocks(state = state, ...),
       "pumas" = tigris::pumas(state = state, ...),
-      "voting districts" = tigris::voting_districts(state = state, ...)
+      "voting districts" = tigris::voting_districts(state = state, ...),
+      "roads" = tigris::roads(state = state, ...),
+      "primary secondary roads" = tigris::primary_secondary_roads(state = state, ...),
+      "area water" = tigris::area_water(state = state, ...),
+      "linear water" = tigris::linear_water(state = state, ...),
+      "landmarks" = tigris::landmarks(state = state, ...),
+      "zctas" = tigris::zctas(state = state, ...)
     )
 
   if (type %in% api_data) {
     data <- janitor::clean_names(data)
   }
 
-  data <- data[lookup_tigris_name(name, data), ]
+  if (!is.null(name)) {
+    data <- data[lookup_tigris_name(name, data), ]
+  }
 
   data <- format_md_sf(data, crs = crs, erase_water = erase_water, clean_names = FALSE)
 
