95ae0f0cb7f7e93eb98b0396f3101b4eaab7b1cf
Eli Pousson <eli.pousson@gmail.com>
2022-07-15 15:05:55 -0400

    refactor: update format_md_data to work w/ getdata

--- R/format_md_data.R
+++ R/format_md_data.R
@@ -5,45 +5,52 @@
 #' names with [janitor::clean_names]. [format_md_crash_data] works with a data
 #' frame of crash data from [get_md_crash_data]
 #'
-#' @param data A `sf` object or data frame object ([format_md_crash_data] only).
-#' @param crs Coordinate reference system passed to [sf::st_transform], Default:
-#'   3857 or option "mapmaryland.crs" if set. Set crs using
+#' @param data A `sf` object or for ([format_md_crash_data] only) a data frame object.
+#' @param crs coordinate reference system. Defaults to
+#'   getOption("mapmaryland.crs", default = 3857). This option can be set with
 #'   [set_mapmaryland_options].
-#' @param erase_water If `TRUE`, use [overedge::st_erase] to erase any geometry
-#'   intersecting with [md_water]; Default: `FALSE`
-#' @param clean_names If `TRUE`, use [janitor::clean_names]; Default: `TRUE`
-#' @inheritParams overedge::format_data
+#' @param erase_water If `TRUE`, erase any geometry intersecting with
+#'   [md_water]; Default: `FALSE`. If TRUE, and erase_data is not `NULL`, [md_water]
+#'   is combined and unioned with erase_data.
+#' @inheritParams getdata::format_sf_data
 #' @return A modified version of the input simple feature data.
 #' @rdname format_md_sf
-#' @seealso [overedge::format_data]
+#' @seealso [getdata::format_sf_data]
 #' @export
-#' @importFrom overedge is_sf is_same_crs st_erase rename_sf_col
-#' @importFrom sf st_transform st_is_valid st_make_valid
-#' @importFrom janitor clean_names
-format_md_sf <- function(data, crs = getOption("mapmaryland.crs", default = 3857), erase_water = FALSE, clean_names = TRUE, sf_col = "geometry") {
-  stopifnot(
-    overedge::is_sf(data)
-  )
-
-  if (clean_names) {
-    data <- janitor::clean_names(data)
-  }
-
-  if (!overedge::is_same_crs(data, crs)) {
-    data <- sf::st_transform(data, crs)
-  }
-
+#' @importFrom sfext st_filter_ext check_sf
+#' @importFrom sf st_union st_combine
+#' @importFrom getdata format_sf_data
+format_md_sf <- function(data,
+                         crs = getOption("mapmaryland.crs", default = 3857),
+                         erase_water = FALSE,
+                         erase_data = NULL,
+                         clean_names = TRUE,
+                         dTolerance = NULL,
+                         smooth = FALSE,
+                         sf_col = "geometry",
+                         ...) {
   if (erase_water) {
-    data <- overedge::st_erase(data, mapmaryland::md_water)
+    water <- sfext::st_filter_ext(md_water, data)
+
+    if (is.null(erase_data)) {
+      erase_data <- water
+    } else {
+      sfext::check_sf(erase_data)
+
+      erase_data <- sf::st_union(sf::st_combine(erase_data, water))
+    }
   }
 
-  if (!sf::st_is_valid(data)) {
-    data <- sf::st_make_valid(data)
-  }
-
-  data <- overedge::rename_sf_col(data, sf_col = "geometry")
-
-  return(data)
+  getdata::format_sf_data(
+    data = data,
+    crs = crs,
+    erase_data = erase_data,
+    clean_names = clean_names,
+    dTolerance = dTolerance,
+    smooth = smooth,
+    sf_col = sf_col,
+    ...
+    )
 }
 
 #' @name format_md_crash_data
@@ -51,9 +58,10 @@ format_md_sf <- function(data, crs = getOption("mapmaryland.crs", default = 3857
 #' @param data Data frame with Maryland vehicular crash data; typically from `get_md_crash_data`
 #' @param drop_code If TRUE, drop all columns that end with "code"
 #' @export
+#' @importFrom rlang check_installed
 format_md_crash_data <- function(data, drop_code = TRUE) {
-  overedge:::is_pkg_installed("dplyr")
-  overedge:::is_pkg_installed("lubridate")
+  rlang::check_installed("dplyr")
+  rlang::check_installed("lubridate")
 
   data <-
     dplyr::mutate(
